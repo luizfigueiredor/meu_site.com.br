dc3c892d8b6e57057299ccc4e169e259d3fdb62a
LUIZ_FRANCISCO <luiz.f.ramalho6@aluno.senai.br>
2025-08-14 10:21:10 -0300

    inicio do projeto

---
+++ resto_divisao.php
@@ -0,0 +1,12 @@
+<?php
+    $numero1 = 10;
+    $numero2 = 3;
+    $resto = $numero1  % $numero2 ."<br>";
+    echo $resto;
+
+    //----------------------------------------------
+
+    echo "Divisão:".$numero1."/".$numero2."=".$numero1/$numero2 ."<br>";
+    echo "Resto:".$numero1%$numero2 . "<br>"; 
+    echo "☆".  "<br>"
+    ?>
